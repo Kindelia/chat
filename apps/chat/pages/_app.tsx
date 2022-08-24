7df8f95351d04a653b904ff03b542aeb708bb273
Gabriel Del <gabrieldelc.barros@gmail.com>
2022-08-24 14:12:49 -0300

    join next-auth-example

--- apps/chat/pages/_app.tsx
+++ apps/chat/pages/_app.tsx
@@ -1,16 +1,22 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 import axios from "../services/httpService";
 
-function MyApp({ Component, pageProps }: AppProps) {
+import { SessionProvider } from "next-auth/react";
+import "./styles.css";
+
+import type { AppProps } from "next/app";
+
+// Use of the <SessionProvider> is mandatory to allow components that call
+// `useSession()` anywhere in your application to access the `session` object.
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig
-      value={{ fetcher: (url: string) => axios(url).then((r) => r.data) }}
-    >
-      <Component {...pageProps} />
-    </SWRConfig>
+    <SessionProvider session={pageProps.session} refetchInterval={0}>
+      <SWRConfig
+        value={{ fetcher: (url: string) => axios(url).then((r) => r.data) }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
+    </SessionProvider>
   );
 }
-
-export default MyApp;
