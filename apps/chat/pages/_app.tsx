2006bde5ef77e61119d16067b5d0dcc64654439f
Gabriel Del <gabrieldelc.barros@gmail.com>
2022-08-25 06:56:23 -0300

    Revert "join next-auth-example"
    This reverts commit 7df8f95351d04a653b904ff03b542aeb708bb273.

--- apps/chat/pages/_app.tsx
+++ apps/chat/pages/_app.tsx
@@ -1,22 +1,16 @@
 import "../styles/globals.css";
+import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 import axios from "../services/httpService";
 
-import { SessionProvider } from "next-auth/react";
-import "./styles.css";
-
-import type { AppProps } from "next/app";
-
-// Use of the <SessionProvider> is mandatory to allow components that call
-// `useSession()` anywhere in your application to access the `session` object.
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session} refetchInterval={0}>
-      <SWRConfig
-        value={{ fetcher: (url: string) => axios(url).then((r) => r.data) }}
-      >
-        <Component {...pageProps} />
-      </SWRConfig>
-    </SessionProvider>
+    <SWRConfig
+      value={{ fetcher: (url: string) => axios(url).then((r) => r.data) }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
   );
 }
+
+export default MyApp;
--- apps/chat/pages/api/auth/[...nextauth].ts
+++ apps/chat/pages/api/auth/[...nextauth].ts
@@ -1,74 +1,39 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
-import GithubProvider from "next-auth/providers/github"
-import TwitterProvider from "next-auth/providers/twitter"
-import Auth0Provider from "next-auth/providers/auth0"
-// import AppleProvider from "next-auth/providers/apple"
-// import EmailProvider from "next-auth/providers/email"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "database/src/client";
 
-// For more information on each option (and a full list of options) go to
-// https://next-auth.js.org/configuration/options
-export const authOptions: NextAuthOptions = {
-  // https://next-auth.js.org/configuration/providers/oauth
+export const authOptions = {
   providers: [
-    /* EmailProvider({
-         server: process.env.EMAIL_SERVER,
-         from: process.env.EMAIL_FROM,
-       }),
-    // Temporarily removing the Apple provider from the demo site as the
-    // callback URL for it needs updating due to Vercel changing domains
-
-    Providers.Apple({
-      clientId: process.env.APPLE_ID,
-      clientSecret: {
-        appleId: process.env.APPLE_ID,
-        teamId: process.env.APPLE_TEAM_ID,
-        privateKey: process.env.APPLE_PRIVATE_KEY,
-        keyId: process.env.APPLE_KEY_ID,
-      },
-    }),
-    */
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-      httpOptions: {
-        timeout: 40000,
-      },
-    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       httpOptions: {
         timeout: 40000,
       },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET,
-    }),
-    Auth0Provider({
-      clientId: process.env.AUTH0_ID,
-      clientSecret: process.env.AUTH0_SECRET,
-      issuer: process.env.AUTH0_ISSUER,
-      httpOptions: {
-        timeout: 40000,
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          username: profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        };
       },
     }),
   ],
-  theme: {
-    colorScheme: "light",
-  },
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
   callbacks: {
-    async jwt({ token }) {
-      token.userRole = "admin"
-      return token
-    },
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+        username: user.username,
+      },
+    }),
   },
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
