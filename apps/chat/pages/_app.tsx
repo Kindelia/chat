ca49f1e0ed3ee65127268c0baa0387d80d2dd257
Gabriel Del <gabrieldelc.barros@gmail.com>
2022-08-27 13:22:47 -0300

    rm other pages

--- apps/chat/pages/_app.tsx
+++ apps/chat/pages/_app.tsx
@@ -3,16 +3,19 @@ import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 import axios from "../services/httpService";
 import { SessionProvider } from "next-auth/react";
+import Header from "chat/components/header";
+
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session} refetchInterval={0}>
-    <SWRConfig
-      value={{ fetcher: (url: string) => axios(url).then((r) => r.data) }}
-    >
-      <Component {...pageProps} />
-    </SWRConfig>
+      <SWRConfig
+        value={{ fetcher: (url: string) => axios(url).then((r) => r.data) }}
+      >
+        <Header />
+        <Component {...pageProps} />
+      </SWRConfig>
     </SessionProvider>
   );
 }
