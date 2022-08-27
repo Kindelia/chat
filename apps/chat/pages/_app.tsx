20de60f0bfeb750dc57f82aabd1a15e3c21cef71
Gabriel Del <gabrieldelc.barros@gmail.com>
2022-08-27 13:06:01 -0300

    rm footer and next-auth-example style

--- apps/chat/pages/_app.tsx
+++ apps/chat/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 import axios from "../services/httpService";
 import { SessionProvider } from "next-auth/react";
-import "./styles.css";
 
 
 export default function App({ Component, pageProps }: AppProps) {
